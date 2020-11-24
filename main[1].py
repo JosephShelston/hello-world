f14c325a123ad779f5f4ed7f26e77d2d0b345cf9
JosephShelston <74607351+JosephShelston@users.noreply.github.com>
2020-11-24 09:53:50 -0500

    Add files via upload

---
+++ main[1].py
@@ -0,0 +1,18 @@
+#intro
+print("you wake up in an airport")
+print("there is no one around")
+
+print("you reach for your backpack but it's completely empty, you walk around and you catch a glimpse of the last airplane leaving.")
+
+print("you walk over to an empty restuarant and notice there is food still there and you are starving")
+
+#decision
+print("[1] Take as much food as you can")
+print("[2] Don't take anything and walk away")
+
+choice = input("Enter a choice: ")
+
+if choice == "1":
+  print("you hear someone walking in the airport")
+elif choice == "2":
+  print("you leave the restuarant in hopes of finding out where you are and how you got here")
